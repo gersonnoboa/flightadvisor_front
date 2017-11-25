a0c6b11c9c710cbf36ef1c6e8699380864789c49
Gerson Noboa <gerson.noboa@ut.ee>
2017-11-25 17:53:10 +0200

    Facebook login popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FacebookService, InitParams, LoginResponse } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private fb: FacebookService) {
+
+	  const params: InitParams = {
+	  	appId      : '421300974954114',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.11'
+	  };
+
+    fb.init(params);
+	}
+
+  loginWithFacebook(): void{
+    this.fb.login().then((response: LoginResponse) => console.log(response)).catch((error: any) => console.error(error));
+  }
 }
+
